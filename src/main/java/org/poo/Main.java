ed830752b474d72e17ad287c629509c06a5ddcf7
FineiasAntonio <fineias1187@gmail.com>
2026-05-26 13:31:49 -0300

    :wrench: testa config database

--- src/main/java/org/poo/Main.java
+++ src/main/java/org/poo/Main.java
@@ -1,11 +1,24 @@
 package org.poo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.poo.config.DatabaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
+        Connection c;
+        try {
+            c = DatabaseConfig.getConnection();
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso! " + c.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(Main.class, args);
     }
 }
